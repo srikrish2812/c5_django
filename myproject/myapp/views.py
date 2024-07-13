184c6c7ab6a4750e275c97f9108ba74eca87a700
abhay <abhay2001sri@gmail.com>
2024-07-14 00:36:49 +0530

    chore: wip

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the Little Lemon restaurant! Hello Customer")
+    return HttpResponse("Welcome to the Little Lemon restaurant! Hello Customer!")
+
+def menu(request):
+    return HttpResponse("This is the Menu")
