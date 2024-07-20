f66ccb143fc232403e292f08f24a347c8164f4e1
abhay <abhay2001sri@gmail.com>
2024-07-20 10:55:57 +0530

    chore: wip

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,7 +4,25 @@ from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the Little Lemon restaurant! Hello Customer!")
+    path = request.path
+    scheme = request.scheme
+    path_info = request.path_info
+    method = request.method
+    user_agent = request.META["HTTP_USER_AGENT"]
+    
+    response = HttpResponse()
+    response.headers['Age'] = 20
+    
+    msg = f"""<br>
+    <br> Path: {path}
+    <br> Scheme: {scheme}
+    <br> Path info: {path_info}
+    <br> Method: {method}
+    <br> User agent: {user_agent}
+    <br> Response header: {response.headers}
+    """
+    
+    return HttpResponse(msg, content_type='text/html',charset='utf-8')
 
 def menu(request):
     return HttpResponse("This is the menu")
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('little/', include("myapp.urls")),
+    path('main/', include("myapp.urls")),
 ]
