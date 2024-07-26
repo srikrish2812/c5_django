7465d2075d8e5abce4b536211e21925183386075
abhay <abhay2001sri@gmail.com>
2024-07-26 06:48:18 +0530

    chore: modified models and migrated them

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,7 +3,7 @@ from unicodedata import name
 
 # Create your models here.
 class Menu(models.Model):
-    name = models.CharField(max_length = 100)
+    dish = models.CharField(max_length = 100)
     cuisine = models.CharField(max_length = 100)
     price = models.IntegerField()
     
