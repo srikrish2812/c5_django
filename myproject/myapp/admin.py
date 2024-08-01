d3c6e0972f83cd01d67b23a95df01b36c5c7c804
abhay <abhay2001sri@gmail.com>
2024-08-01 08:01:54 +0530

    chore: django admin wip

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -4,3 +4,4 @@ from . import models
 # Register your models here.
 # admin.site.register(models.Menu)
 admin.site.register(models.Logger)
+admin.site.register(models.Reservation)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,3 +5,13 @@ class Logger(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     time_log = models.TimeField(help_text="Enter the exact text")
+    
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    contact = models.CharField("Phone number",max_length=300)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.CharField(max_length=300, blank=True)
+    
+    
